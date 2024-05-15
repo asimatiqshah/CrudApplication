7a6a9019316cb9241a9709421b6538620bb15cee
asimshah <shah.asimatiq@gmail.com>
2024-05-15 19:02:40 +0500

    UI Design - Todolists header and list UI Updated

--- components/TodLists.js
+++ components/TodLists.js
@@ -1,21 +1,43 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Button, Image, StyleSheet, Text, View} from 'react-native';
 
 const TodLists = () => {
   return (
     <View style={styles.bodyArea}> 
       <View style={styles.topheader}>
-        <View style={{flex:1,justifyContent:'center',alignItems:'center',backgroundColor:'red'}}>
-          <Text>arrow</Text>
+        <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+        <Image
+            source={require('../src/assets/left-arrow.png')}
+            style={{width:24,height:24}}
+          />
         </View>
         <View style={{flex:2,justifyContent:'center',alignItems:'center'}}>
           <Text style={{fontSize:20,color:'black',fontWeight:'700'}}>TodoList</Text>
         </View>
-        <View style={{flex:1,justifyContent:'center',alignItems:'center',backgroundColor:'red'}}>
-          <Text>Right Arrow</Text>
+        <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+          <Image
+            source={require('../src/assets/three-dots-vertical.png')}
+            style={{width:24,height:24}}
+          />
         </View>
       </View>
-      <View style={{backgroundColor:'red',flex:1}}>
-        <Text>Today</Text>
+      <View style={{backgroundColor:'#151515',flex:1}}>
+        <Text style={{color:'white'}}>Today</Text>
+        <Text style={{fontSize:14,color:'#7D7878'}}>March 4, 2010</Text>
+        <View style={{backgroundColor:'#201F1F',height:50,justifyContent:'center',alignItems:'flex-start'}}>
+          <Text style={{color:'white'}}>AsimShah</Text>
+        </View>
+        <View style={{backgroundColor:'grey',height:50,justifyContent:'center',alignItems:'center'}}>
+          <Text>AsimShah</Text>
+        </View>
+        <View style={{backgroundColor:'grey',height:50,justifyContent:'center',alignItems:'center'}}>
+          <Text>AsimShah</Text>
+        </View>
+      </View>
+      <View style={styles.circle}>
+      <Image
+        source={require('../src/assets/plus.png')}
+        style={{width:25,height:25}}
+      />
       </View>
     </View>
   );
@@ -34,5 +56,15 @@ const styles = StyleSheet.create({
     flex : 1,
     backgroundColor:'black',
     // height:'100%'
-  }
+  },circle :{
+    height: 60,
+    width: 60,
+    borderRadius: 30,
+    backgroundColor:'grey',
+    position:'absolute',
+    bottom:0,
+    right :0,
+    justifyContent:'center',
+    alignItems:'center'
+   }
 });
