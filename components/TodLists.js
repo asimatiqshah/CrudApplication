44e68799ef06b3d9382b9c3a077b1aeb1c821a63
asimshah <shah.asimatiq@gmail.com>
2024-05-24 22:42:35 +0500

    complete crud application

--- components/TodLists.js
+++ components/TodLists.js
@@ -1,8 +1,9 @@
-import { useEffect, useState } from 'react';
-import { Button, FlatList, Image, Modal, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { useCallback, useEffect, useState } from 'react';
+import { Button, FlatList, Image, Modal, Pressable, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import CheckBox from 'react-native-check-box'
 import { useDispatch, useSelector } from 'react-redux';
-import { showData, updateStatus } from '../src/redux/reducers/crudSlice';
+import { addItemToList, deleteItem, showData, updateStatus } from '../src/redux/reducers/crudSlice';
+
 const TodLists = () => {
 
   const dispatch = useDispatch();
@@ -13,27 +14,12 @@ const TodLists = () => {
   }
 
   const initialData = [
-    {
-      id: 1,
-      task: "Do exercise",
-      date: "6:00",
-      status: false
-    },
-    {
-      id: 2,
-      task: "Buy Vegetables",
-      date: "6:00",
-      status: false
-    },
-    {
-      id: 3,
-      task: "Go To Shopping",
-      date: "6:00",
-      status: true,
-    }
+   
   ]
   const [todoList, setTodoList] = useState(initialData);
-  const [isModal,setIsModal] = useState(true);
+  const [inputVal, setInputVal] = useState("")
+  const [isModal, setIsModal] = useState(false);
+  const [recentDate, setRecentDate] = useState("");
 
   const fetchUserData = () => {
     // dispatch(todoList);
@@ -50,10 +36,70 @@ const TodLists = () => {
     updatedItem && dispatch(updateStatus(updatedItem));
   }
 
+  const addNewHandler = () => {
+    const todayDate = new Date();
+    const curTime = `${todayDate.getHours()}:${todayDate.getMinutes()}`;
+    let newItem = {
+      value: inputVal,
+      currentDate: recentDate,
+      curTime
+    }
+
+    inputVal.trim().length > 4 && dispatch(addItemToList(newItem));
+    setInputVal("");
+    setIsModal(false);
+  }
+
+  const currentDate = () => {
+    const todayDate = new Date();
+    const monthNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+    const cur_Month = todayDate.getMonth();
+    const today_Date = `${monthNames[cur_Month]} ${todayDate.getDate()},${todayDate.getFullYear()}`;
+    setRecentDate(today_Date);
+  }
+
+  //List Items Code in UseCallback
+  const renderItem = useCallback(({item}) => {
+    return (
+      <View style={{ backgroundColor: '#201F1F', height: 50, alignItems: 'flex-start', flexDirection: 'row', alignItems: 'center', paddingLeft: 20, paddingRight: 20, marginTop: 20 }}>
+        <TouchableOpacity onPress={()=>dispatch(deleteItem(item.id))}>
+          <Image 
+          source={require('../src/assets/delete.png')}
+          style={{width:20,height:20,marginRight:10}}
+          />
+        </TouchableOpacity>
+        <CheckBox
+          onClick={() => updateTodoList(item)}
+          leftText={"CheckBox"}
+          isChecked={item.status}
+          checkBoxColor={item.status ? colorList.colorWhite : colorList.colorDark}
+        />
+        <Text style={{ flex: 2, color: item.status ? colorList.colorWhite : colorList.colorDark, paddingLeft: 10, textDecorationLine: item.status ? 'none' : 'line-through', textDecorationStyle: 'solid', fontSize: 20 }}>{item.task}</Text>
+        <Text style={{ justifyContent: 'flex-end', color: item.status ? colorList.colorWhite : colorList.colorDark, fontSize: 14 }}>{item.date}</Text>
+      </View>
+    )
+  }, [fetchReduxData])
+
+
   useEffect(() => {
     fetchUserData();
+    currentDate();
   }, [])
   const fetchReduxData = useSelector(({ crudSlice }) => crudSlice.reduxTodoList);
+  console.log(fetchReduxData);
   return (
 
 
@@ -80,57 +126,54 @@ const TodLists = () => {
         <View style={{ backgroundColor: '#151515', flex: 1 }}>
           <View style={{ marginTop: 20, marginLeft: 20 }}>
             <Text style={{ color: 'white' }}>Today</Text>
-            <Text style={{ fontSize: 14, color: '#7D7878' }}>March 4, 2010</Text>
+            <Text style={{ fontSize: 14, color: '#7D7878' }}>{recentDate}</Text>
           </View>
 
           {/* Todo 1 */}
 
 
           {
-            fetchReduxData &&
-            <FlatList
-              data={fetchReduxData}
-              renderItem={({ item }) => {
-                return (
-                  <View style={{ backgroundColor: '#201F1F', height: 50, alignItems: 'flex-start', flexDirection: 'row', alignItems: 'center', paddingLeft: 20, paddingRight: 20, marginTop: 20 }}>
-                    <CheckBox
-                      onClick={() => updateTodoList(item)}
-                      leftText={"CheckBox"}
-                      isChecked={item.status}
-                      checkBoxColor={item.status ? colorList.colorWhite : colorList.colorDark}
-                    />
-                    <Text style={{ flex: 2, color: item.status ? colorList.colorWhite : colorList.colorDark, paddingLeft: 10, textDecorationLine: item.status ? 'none' : 'line-through', textDecorationStyle: 'solid', fontSize: 20 }}>{item.task}</Text>
-                    <Text style={{ justifyContent: 'flex-end', color: item.status ? colorList.colorWhite : colorList.colorDark, fontSize: 15 }}>{item.date}</Text>
-                  </View>
 
-                )
-              }}
-            />
+            (fetchReduxData.length > 0)
+              ?
+              <FlatList
+                data={fetchReduxData}
+                renderItem={renderItem}
+              />
+              :
+              <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+                <Text style={{ color: 'white', fontSize: 30 }}>No Data</Text>
+              </View>
           }
 
         </View>
-        <TouchableOpacity style={styles.circle} onPress={()=>setIsModal(true)}>
+        <TouchableOpacity style={styles.circle} onPress={() => setIsModal(true)}>
           <Image
             source={require('../src/assets/plus.png')}
             style={{ width: 25, height: 25 }}
           />
         </TouchableOpacity>
       </View>
-
+          
       <View style={{ justifyContent: 'center', }}>
         <Modal visible={isModal} transparent>
-          
-          <TouchableOpacity style={{ flex: 1, backgroundColor: 'rgba(0,0,0,0.5)', justifyContent: 'flex-end', alignItems: 'center' }} onPress={()=>setIsModal(false)}>
+
+          <TouchableOpacity style={{ flex: 1, backgroundColor: 'rgba(0,0,0,0.5)', justifyContent: 'flex-end', alignItems: 'center' }} onPress={() => setIsModal(false)}>
             {/* contentBox */}
-            <View style={{ width: '100%', height: 300, backgroundColor: 'white', borderTopRightRadius:20,borderTopLeftRadius:20, alignItems: 'center',paddingTop:15 }}>
+            <View style={{ width: '100%', height: 300, backgroundColor: 'white', borderTopRightRadius: 20, borderTopLeftRadius: 20, alignItems: 'center', paddingTop: 15 }}>
               <Image
-              source={require('../src/assets/gap.png')}
-              style={{width:'50%',borderRadius:250}}
+                source={require('../src/assets/gap.png')}
+                style={{ width: '50%', borderRadius: 250 }}
               />
-              <Text style={{color:'black',fontSize:25,fontWeight:'700',marginBottom:20,marginTop:40}}>Enter Your New Item</Text>
-              <TextInput style={{borderColor:'black',borderWidth:1,width: "95%",borderRadius:250,paddingLeft:20,fontWeight:'600'}} placeholder='Enter Your Item' placeholderTextColor="grey" keyboardType='default' />
-              <TouchableOpacity style={{backgroundColor:'black',width:'95%',height:60,borderRadius:250,justifyContent:'center',alignItems:'center',marginTop:20}}>
-                <Text style={{fontSize:18,color:'white'}}>SAVE</Text>
+              <Text style={{ color: 'black', fontSize: 25, fontWeight: '700', marginBottom: 20, marginTop: 40 }}>Enter Your New Item</Text>
+              <TextInput
+                value={inputVal}
+                onChangeText={(val) => (setInputVal(val))}
+                style={{ borderColor: 'black', color: 'black', borderWidth: 1, width: "95%", borderRadius: 250, paddingLeft: 20, fontWeight: '600' }} placeholder='Enter Your Item' placeholderTextColor="grey" keyboardType='default' />
+              <TouchableOpacity
+                onPress={addNewHandler}
+                style={{ backgroundColor: 'black', width: '95%', height: 60, borderRadius: 250, justifyContent: 'center', alignItems: 'center', marginTop: 20 }}>
+                <Text style={{ fontSize: 18, color: 'white' }}>SAVE</Text>
               </TouchableOpacity>
             </View>
           </TouchableOpacity>
--- src/redux/reducers/crudSlice.js
+++ src/redux/reducers/crudSlice.js
@@ -25,34 +25,62 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-    value : 0,
-    reduxTodoList : []
+    value: 0,
+    reduxTodoList: []
 }
 
 const crudSlice = createSlice({
-    name : 'curdApplication',
+    name: 'curdApplication',
     initialState,
-    reducers : {
-        increment : (state,action)=>{
+    reducers: {
+        increment: (state, action) => {
             // console.log("Action1 ", action)
         },
-        showData : (state,action)=>{
-            const {payload} = action;
+        showData: (state, action) => {
+            const { payload } = action;
             state.reduxTodoList = [...payload];
             // console.log(state.todoList)
         },
-        updateStatus : (state,action)=>{
-            //Updated Object
-            let {payload} = action;
-            //Index number of object
-             const indexVal = state.reduxTodoList.findIndex((item)=> item.id == payload.id );
-             const cloneList = [...state.reduxTodoList];
-             cloneList.splice(indexVal,1,payload);
-             state.reduxTodoList = [...cloneList];
+        addItemToList: (state, action) => {
+            const { payload } = action;
+            console.log(payload);
 
+            //Generate New ID By Date Function 
+            const generateUniqueID = Math.floor(Math.random() * 1000);
+            //fetch last 3 digit of currentDate
+            let cur_date = Date.now().toString();
+            let newID = cur_date.slice(-3) + generateUniqueID.toString();
+
+            //make new object
+            let newItem = {
+                id: newID,
+                task: payload.value,
+                date: `${payload.curTime} | ${payload.currentDate}`,
+                status: true
+            }
+            state.reduxTodoList = [
+                ...state.reduxTodoList,
+                newItem
+            ]
+            state.reduxTodoList
+        }
+        ,
+        updateStatus: (state, action) => {
+            //Updated Object
+            let { payload } = action;
+            //Index number of object
+            const indexVal = state.reduxTodoList.findIndex((item) => item.id == payload.id);
+            const cloneList = [...state.reduxTodoList];
+            cloneList.splice(indexVal, 1, payload);
+            state.reduxTodoList = [...cloneList];
+        },
+        deleteItem:(state,action)=>{
+            let {payload} = action;
+            let result = state.reduxTodoList.filter((item)=> item.id !== payload);
+            state.reduxTodoList = [...result];
         }
     }
 })
 
-export const {increment,showData,updateStatus} = crudSlice.actions;
+export const { increment, showData, updateStatus, addItemToList,deleteItem } = crudSlice.actions;
 export default crudSlice.reducer;
